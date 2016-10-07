89da20abcc8fdcbdab1cf8821db2658a71be8976
JessieStam <jessiestam@quicknet.nl>
2016-10-07 10:40:18 +0200

    Initial commit

---
+++ app/src/main/java/jessie_stam/jessiestam_pset5_jaar2_desktop/TodoFragment.java
@@ -0,0 +1,58 @@
+package jessie_stam.jessiestam_pset5_jaar2_desktop;
+
+import android.app.ListFragment;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * Created by Jessie on 07/10/2016.
+ */
+
+@RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+public class TodoFragment extends ListFragment {
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+}
